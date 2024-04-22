cd00bc44071018fc58b8f1117688eb105b2191b0
Paul-Franck Dencausse <121952248+Paul-FranckDencausse@users.noreply.github.com>
2024-04-22 11:38:23 +0200

    Update index.php

--- exo1/index.php
+++ exo1/index.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = ""; 
 $dbname = "injectionsql";
 
-// Connexion à la base de données
+// Connexion à la BD
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Vérification de la connexion
@@ -12,11 +12,11 @@ if ($conn->connect_error) {
   die("Échec de la connexion : " . $conn->connect_error);
 }
 
-// Récupération des données utilisateur
-$nom_utilisateur = $_POST['nom_utilisateur'];
-$mot_de_passe = $_POST['mot_de_passe'];
+// Récupération des données  de l'utilisateur
+$nom_utilisateur = $conn->real_escape_string($_POST['nom_utilisateur']); // Pour contrer l'injection il faut échapper les caractères. C'est à dire que le client ne puisse pas les reconnaître comme du code. Juste comme du texte.
+$mot_de_passe = $conn->real_escape_string($_POST['mot_de_passe']); // Utilisation de real_escape_string pour échapper les caractères spéciaux
 
-// Requête SQL vulnérable à l'injection
+// Requête SQL sécurisée
 $query = "SELECT * FROM utilisateurs WHERE nom_utilisateur = '$nom_utilisateur' AND mot_de_passe = '$mot_de_passe'";
 
 // Exécution de la requête

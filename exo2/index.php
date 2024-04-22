8a300114fc6b110acb4cd2edc3a41a0b1dcd04a7
Paul-Franck Dencausse <121952248+Paul-FranckDencausse@users.noreply.github.com>
2024-04-22 11:46:12 +0200

    Update index.php

--- exo2/index.php
+++ exo2/index.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_GET['search'])) {
-    $searchTerm = $_GET['search'];
+    $searchTerm = htmlspecialchars($_GET['search']); // Pour éviter que le client pense que cette entrée est du code on échappe les caractères à l'aide de la fonction native htmlspecialchars
     echo "Résultats de la recherche pour : " . $searchTerm;
 } else {
     echo "Utiliser l'url pour afficher quelque chose";
